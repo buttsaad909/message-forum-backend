10e7497729aa7ce812a2f07cc7731cbd46d26a62
Saad Butt <khawajamuhammadsaad.butt@glasgow.ac.uk>
2023-11-14 15:18:26 +0000

    Setting up MongoDB connection

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+var mongoose = require('mongoose');
 
 app.use(express.json())
 app.use(cors());
 
-const PORT = 3010;
-
-app.listen(PORT, () => {
-    console.log(`Server Listening on port ${PORT}`);
+const Schema = new mongoose.Schema({
+    author: String,
+    content: String,
+    timestamp: { type: Date, default: Date.now },
+    comments: [{
+      author: String,
+      content: String,
+      timestamp: { type: Date, default: Date.now }
+    }]
   });
+  
+const Message = mongoose.model('Message', Schema);
+
+const uri = "mongodb+srv://Saad:chemistry909@cluster0.cl4dg.mongodb.net/Message";
+mongoose.connect(uri);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+// Connection successful
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+
+    const PORT = 3010;
+    app.listen(PORT, () => {
+        console.log(`Server Listening on port ${PORT}`);
+    });
+});
